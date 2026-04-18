b673308b40246230c77676000a044ca1b934e526
FASUZO <FASUZO@users.noreply.github.com>
2026-04-18 13:08:05 +0000

    deploy: d860f3bb8a48295f8d55acf810c385c0cacaa912

---
+++ sw.js
@@ -0,0 +1,66 @@
+
+const CACHE_NAME = 'bus-tool-cache-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './vite.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          (response) => {
+            if(!response || response.status !== 200) {
+              return response;
+            }
+            // 只缓存静态资源
+            if (event.request.url.includes('/assets/')) {
+              const responseToCache = response.clone();
+              caches.open(CACHE_NAME)
+                .then((cache) => {
+                  cache.put(event.request, responseToCache);
+                });
+            }
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
